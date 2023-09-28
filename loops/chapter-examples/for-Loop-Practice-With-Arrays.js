8586030228230cdf080f2ba5cbc3d5e9789fd898
Nicole Ditto <142694190+nicditto@users.noreply.github.com>
2023-09-28 10:04:31 -0500

    Update9/28

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let names = ["John", "Kenya", "Ty", "Nicole"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,7 @@
 // Create a string variable containing your name.
-
+let name1 = "Nicole"
 
 // Write a for loop that prints each character in your name on a different line.
+for (let i = 0; i < name1.length; i++){
+    console.log(name1[i]);
+}
