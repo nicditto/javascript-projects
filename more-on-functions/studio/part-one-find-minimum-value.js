2e6c6cbb9ac70f899394a291837f1ed2574d435f
Nicole Ditto <nicoleditto@Nicoles-Air.lan>
2023-10-04 18:20:45 -0500

    Updated10/04/23

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -6,5 +6,15 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
+function findMinValue(array) {
+   let min = array[0];
+    for (let i = 1; i < array.length; i++){
+    if(array[i] < min){
+        min = array[i]
+    }
+   
+    }
+    return min;
+}
+console.log(findMinValue(nums1));
 
-console.log(/* your code here */);
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,22 +1,38 @@
-function findMinValue(arr){
-  let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
-      min = arr[i];
-    }
-  }
-  return min;
+
+
+
+;  // create a new empty array
+function sortNumbers(array) {  // create a function
+ let newArray = []
+  while(array.length > 0){
+    let min = findMinValue(array); // us findMinValue function
+    newArray.push(min);  // push into new array
+     array.splice(array.indexOf(min), 1);                      // remove from old array
+  }                       
+return newArray;
 }
+
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 
-/*Within the function:
-1) Define a new, empty array to hold the final sorted numbers.
-2) Use the findMinValue function to find the minimum value in the old array.
-3) Add the minimum value to the new array, and remove the minimum value from the old array.
-4) Repeat parts b & c until the old array is empty.
-5) Return the new sorted array.
-6) Be sure to print the results in order to verify your code.*/
+//Within the function:
+//1) Define a new, empty array to hold the final sorted numbers.
+//2) Use the findMinValue function to find the minimum value in the old array.
+function findMinValue(array) {
+  let min = array[0];
+   for (let i = 1; i < array.length; i++){
+   if(array[i] < min){
+       min = array[i]
+   }
+  
+   }
+   return min;
+   
+}
+//3) Add the minimum value to the new array, and remove the minimum value from the old array.
+//4) Repeat parts b & c until the old array is empty.
+//5) Return the new sorted array.
+//6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
 
@@ -27,3 +43,5 @@ function findMinValue(arr){
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(sortNumbers(nums1));
